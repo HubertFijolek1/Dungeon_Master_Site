52c2da0d8781a9e038e31355215f2a391ea1614f
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-18 09:29:39 +0100

    Set up basic URLs and root views

--- dm_site/dm_site/urls.py
+++ dm_site/dm_site/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for dm_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+def home_view(request):
+    return JsonResponse({"message": "Dungeon Master Website API"}, status=200)
+
+def health_check(request):
+    return JsonResponse({"status": "OK"}, status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home_view, name='home'),
+    path('health/', health_check, name='health_check'),
+    # path('api/', include('api.urls')),  # Placeholder for future APIs
+    # path('campaigns/', include('campaigns.urls')),
+    # path('characters/', include('characters.urls')),
+    # path('world/', include('world.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
