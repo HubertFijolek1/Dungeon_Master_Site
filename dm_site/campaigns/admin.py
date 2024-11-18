9711a1d939583cbba29a4b10d2ab645101f462e6
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-18 09:58:39 +0100

    Register Campaign model in admin

--- dm_site/campaigns/admin.py
+++ dm_site/campaigns/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Campaign
 
-# Register your models here.
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'status', 'created_at')
+    search_fields = ('name', 'owner__username')
+    list_filter = ('status', 'created_at')
+
+admin.site.register(Campaign, CampaignAdmin)
