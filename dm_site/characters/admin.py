71efaeff6828aea76d3ac0de78a5c4dc8971cbb3
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-18 10:03:11 +0100

    Register Character model in admin

--- dm_site/characters/admin.py
+++ dm_site/characters/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Character
 
-# Register your models here.
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'character_type', 'campaign', 'created_at')
+    search_fields = ('name', 'campaign__name')
+    list_filter = ('character_type', 'campaign__status')
+
+admin.site.register(Character, CharacterAdmin)
