29fa46648282a4d0a02656a3abd51f632f8c956b
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-18 09:59:47 +0100

    Register Session model in admin

--- dm_site/campaigns/admin.py
+++ dm_site/campaigns/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Campaign
+from .models import Campaign, Session
 
 class CampaignAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'status', 'created_at')
     search_fields = ('name', 'owner__username')
     list_filter = ('status', 'created_at')
 
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('campaign', 'date', 'time', 'created_at')
+    search_fields = ('campaign__name',)
+    list_filter = ('date', 'campaign__status')
+
 admin.site.register(Campaign, CampaignAdmin)
+admin.site.register(Session, SessionAdmin)
