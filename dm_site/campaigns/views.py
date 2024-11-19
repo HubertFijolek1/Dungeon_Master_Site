1c9d6fba34bfae474dd42566b5aef3f1f9cdea67
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-19 09:43:40 +0100

    Create Campaign views and templates

--- dm_site/campaigns/views.py
+++ dm_site/campaigns/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Campaign, Session, Milestone
+from .models import Campaign, Session, Milestone, Participant
 from .serializers import CampaignSerializer, SessionSerializer, MilestoneSerializer, ParticipantSerializer
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CampaignForm
 
 class CampaignViewSet(viewsets.ModelViewSet):
     queryset = Campaign.objects.all()
@@ -25,3 +28,30 @@ class ParticipantViewSet(viewsets.ModelViewSet):
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+class CampaignListView(ListView):
+    model = Campaign
+    template_name = 'campaigns/campaign_list.html'
+    context_object_name = 'campaigns'
+
+class CampaignDetailView(DetailView):
+    model = Campaign
+    template_name = 'campaigns/campaign_detail.html'
+    context_object_name = 'campaign'
+
+class CampaignCreateView(LoginRequiredMixin, CreateView):
+    model = Campaign
+    form_class = CampaignForm
+    template_name = 'campaigns/campaign_form.html'
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class CampaignUpdateView(LoginRequiredMixin, UpdateView):
+    model = Campaign
+    form_class = CampaignForm
+    template_name = 'campaigns/campaign_form.html'
+
+    def get_queryset(self):
+        return Campaign.objects.filter(owner=self.request.user)
