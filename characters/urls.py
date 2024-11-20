4ae9c4d441c4d31769d0cb9f5dd5a23659206730
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-20 10:17:44 +0100

    Implement Monster API endpoints with CRUD operations.

--- characters/urls.py
+++ characters/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CharacterViewSet,InventoryItemViewSet
+from .views import CharacterViewSet, InventoryItemViewSet, MonsterViewSet
 
 router = DefaultRouter()
 router.register(r'characters', CharacterViewSet)
 router.register(r'inventory-items', InventoryItemViewSet)
+router.register(r'monsters', MonsterViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import Character, InventoryItem
-from .serializers import CharacterSerializer, InventoryItemSerializer
+from .models import Character, InventoryItem, Monster
+from .serializers import CharacterSerializer, InventoryItemSerializer, MonsterSerializer
 
 class CharacterViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all()
@@ -13,4 +13,9 @@ class CharacterViewSet(viewsets.ModelViewSet):
 class InventoryItemViewSet(viewsets.ModelViewSet):
     queryset = InventoryItem.objects.all()
     serializer_class = InventoryItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class MonsterViewSet(viewsets.ModelViewSet):
+    queryset = Monster.objects.all()
+    serializer_class = MonsterSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
