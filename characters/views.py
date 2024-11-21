9b1f9e9cd94cd078148a426b39eb468e8a9aa2ef
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-21 09:53:02 +0100

    Reorder views.py in characters section

--- characters/views.py
+++ characters/views.py
@@ -1,17 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from rest_framework import viewsets, permissions
 from .models import Character, InventoryItem, Monster
 from .serializers import CharacterSerializer, InventoryItemSerializer, MonsterSerializer
-from django.views.generic import ListView, DetailView, CreateView, TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CharacterForm, InventoryItemForm
+from .forms import CharacterForm, InventoryItemForm, MonsterForm
+from campaigns.models import Campaign
+
+# REST Framework ViewSets
 
 class CharacterViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save()
 
 class InventoryItemViewSet(viewsets.ModelViewSet):
     queryset = InventoryItem.objects.all()
@@ -23,6 +25,7 @@ class MonsterViewSet(viewsets.ModelViewSet):
     serializer_class = MonsterSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+# Django Views
 
 class CharacterListView(ListView):
     model = Character
@@ -47,8 +50,12 @@ class CharacterCreateView(LoginRequiredMixin, CreateView):
     template_name = 'characters/character_form.html'
 
     def form_valid(self, form):
-        form.instance.campaign_id = self.kwargs['campaign_id']
+        campaign_id = self.kwargs.get('campaign_id')
+        form.instance.campaign = get_object_or_404(Campaign, id=campaign_id)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('character_detail', kwargs={'pk': self.object.pk})
 
 class InventoryItemListView(ListView):
     model = InventoryItem
@@ -56,7 +63,14 @@ class InventoryItemListView(ListView):
     context_object_name = 'inventory_items'
 
     def get_queryset(self):
-        return InventoryItem.objects.filter(character_id=self.kwargs['character_id'])
+        character_id = self.kwargs['character_id']
+        self.character = get_object_or_404(Character, pk=character_id)
+        return InventoryItem.objects.filter(character=self.character)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['character'] = self.character
+        return context
 
 class InventoryItemCreateView(LoginRequiredMixin, CreateView):
     model = InventoryItem
@@ -64,8 +78,46 @@ class InventoryItemCreateView(LoginRequiredMixin, CreateView):
     template_name = 'characters/inventoryitem_form.html'
 
     def form_valid(self, form):
-        form.instance.character_id = self.kwargs['character_id']
+        character_id = self.kwargs['character_id']
+        form.instance.character = get_object_or_404(Character, pk=character_id)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('inventoryitem_list', kwargs={'character_id': self.object.character.pk})
+
+class MonsterListView(ListView):
+    model = Monster
+    template_name = 'characters/monster_list.html'
+    context_object_name = 'monsters'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        campaign_id = self.kwargs.get('campaign_id')
+        if campaign_id:
+            queryset = queryset.filter(campaign_id=campaign_id)
+        search_query = self.request.GET.get('q', '')
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
+        return queryset
+
+class MonsterDetailView(DetailView):
+    model = Monster
+    template_name = 'characters/monster_detail.html'
+    context_object_name = 'monster'
+
+class MonsterCreateView(LoginRequiredMixin, CreateView):
+    model = Monster
+    form_class = MonsterForm
+    template_name = 'characters/monster_form.html'
+
+    def form_valid(self, form):
+        campaign_id = self.kwargs.get('campaign_id')
+        if campaign_id:
+            form.instance.campaign = get_object_or_404(Campaign, id=campaign_id)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('monster_detail', kwargs={'pk': self.object.pk})
 
 class CharacterDashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'characters/character_dashboard.html'
