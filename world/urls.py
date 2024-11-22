5e70dc77218ab1bdd2069281f4ca4f4b8a74ea26
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-22 12:21:42 +0100

    Implement Lore API endpoints with CRUD operations.

--- world/urls.py
+++ world/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MapViewSet, LocationViewSet
+from .views import MapViewSet, LocationViewSet, LoreViewSet
 
 router = DefaultRouter()
 router.register(r'maps', MapViewSet)
 router.register(r'locations', LocationViewSet)
+router.register(r'lore', LoreViewSet)
 
 app_name = 'world'
 
--- world/views.py
+++ world/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import Map, Location
-from .serializers import MapSerializer, LocationSerializer
+from .models import Map, Location, Lore
+from .serializers import MapSerializer, LocationSerializer, LoreSerializer
 
 class MapViewSet(viewsets.ModelViewSet):
     queryset = Map.objects.all()
@@ -10,4 +10,9 @@ class MapViewSet(viewsets.ModelViewSet):
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LoreViewSet(viewsets.ModelViewSet):
+    queryset = Lore.objects.all()
+    serializer_class = LoreSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
