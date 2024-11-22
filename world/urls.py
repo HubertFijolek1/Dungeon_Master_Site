e191f6fb17cbea557cd9621cd5cc9aaab83a0b87
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-22 12:19:32 +0100

    Implement Location API endpoints with CRUD operations.

--- world/urls.py
+++ world/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MapViewSet
+from .views import MapViewSet, LocationViewSet
 
 router = DefaultRouter()
 router.register(r'maps', MapViewSet)
+router.register(r'locations', LocationViewSet)
 
 app_name = 'world'
 
--- world/views.py
+++ world/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets, permissions
-from .models import Map
-from .serializers import MapSerializer
+from .models import Map, Location
+from .serializers import MapSerializer, LocationSerializer
 
 class MapViewSet(viewsets.ModelViewSet):
     queryset = Map.objects.all()
     serializer_class = MapSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
