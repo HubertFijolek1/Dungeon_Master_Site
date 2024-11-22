458df87d1a3af28930f7b9ab4294c2c90f6285db
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-22 12:13:29 +0100

    Register Location model in admin with customized interface.

--- world/admin.py
+++ world/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Map
+from .models import Map, Location
 
+@admin.register(Map)
 class MapAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at')
     search_fields = ('name',)
     list_filter = ('created_at',)
 
-admin.site.register(Map, MapAdmin)
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'map', 'coordinates', 'created_at')
+    search_fields = ('name', 'map__name')
+    list_filter = ('map', 'created_at')
