a2cf095621028d5d18ec5ab54c735924be0a2743
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-23 11:40:45 +0100

    Register DiceRoll model in admin with customized interface.

--- mechanics/admin.py
+++ mechanics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DiceRoll, Encounter, Loot
 
-# Register your models here.
+@admin.register(DiceRoll)
+class DiceRollAdmin(admin.ModelAdmin):
+    list_display = ('user', 'campaign', 'roll_type', 'result', 'timestamp')
+    search_fields = ('user__username', 'campaign__name', 'roll_type', 'result')
+    list_filter = ('roll_type', 'timestamp', 'campaign')
