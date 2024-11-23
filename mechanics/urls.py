46bc3a900ea1d22999b703742886c578d00975d5
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-23 11:44:25 +0100

    Implement Encounter API endpoints with CRUD operations.

--- mechanics/urls.py
+++ mechanics/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DiceRollViewSet
+from .views import DiceRollViewSet, EncounterViewSet
 
 router = DefaultRouter()
 router.register(r'dice-rolls', DiceRollViewSet)
+router.register(r'encounters', EncounterViewSet)
 
 app_name = 'mechanics'
 
--- mechanics/views.py
+++ mechanics/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import DiceRoll
-from .serializers import DiceRollSerializer
+from .models import DiceRoll, Encounter
+from .serializers import DiceRollSerializer, EncounterSerializer
 
 class DiceRollViewSet(viewsets.ModelViewSet):
     queryset = DiceRoll.objects.all()
@@ -9,3 +9,8 @@ class DiceRollViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class EncounterViewSet(viewsets.ModelViewSet):
+    queryset = Encounter.objects.all()
+    serializer_class = EncounterSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
