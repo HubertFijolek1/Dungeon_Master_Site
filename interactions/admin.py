a2b55103be33d121a446ad4745b690d8175271a3
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-26 17:33:46 +0100

    Register Message model in admin with customized interface.

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message, ForumPost, Poll, PollOption, PollVote
 
-# Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'short_content', 'timestamp')
+    search_fields = ('sender__username', 'receiver__username', 'content')
+    list_filter = ('timestamp',)
+
+    def short_content(self, obj):
+        return obj.content[:50]
+    short_content.short_description = 'Content'
