7d7599e8b0a0e7b97e76da47af029524b7bd5741
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-27 20:37:40 +0100

    .

--- dm_site/urls.py
+++ dm_site/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('player/dashboard/', PlayerDashboardView.as_view(), name='player_dashboard'),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
