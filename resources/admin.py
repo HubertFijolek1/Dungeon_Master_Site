4d9f6c0f36443a6d533b04a775f18675b940d5a5
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-02 12:42:26 +0100

    Register Item model in admin with customized interface.

--- resources/admin.py
+++ resources/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Spell, Ability
+from .models import Spell, Ability, Item
 
 @admin.register(Spell)
 class SpellAdmin(admin.ModelAdmin):
@@ -12,3 +12,9 @@ class AbilityAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'related_spell', 'created_at')
     search_fields = ('name', 'description')
     list_filter = ('type', 'created_at')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'rarity', 'value', 'created_at')
+    search_fields = ('name', 'description')
+    list_filter = ('type', 'rarity', 'created_at')
