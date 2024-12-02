d789628c473265e58e9531a5769b3b96392a7c25
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-02 12:42:52 +0100

    Register AdventureModule model in admin with customized interface.

--- resources/admin.py
+++ resources/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Spell, Ability, Item
+from .models import Spell, Ability, Item, AdventureModule
 
 @admin.register(Spell)
 class SpellAdmin(admin.ModelAdmin):
@@ -18,3 +18,9 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'rarity', 'value', 'created_at')
     search_fields = ('name', 'description')
     list_filter = ('type', 'rarity', 'created_at')
+
+@admin.register(AdventureModule)
+class AdventureModuleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'description', 'content', 'author__username')
+    list_filter = ('created_at', 'author')
