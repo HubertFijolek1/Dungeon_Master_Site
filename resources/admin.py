fd9c665c55f061cd0d3a71d056bd09bd63124ddd
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-02 12:42:05 +0100

    Register Ability model in admin with customized interface.

--- resources/admin.py
+++ resources/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Spell
+from .models import Spell, Ability
 
 @admin.register(Spell)
 class SpellAdmin(admin.ModelAdmin):
     list_display = ('name', 'level', 'school', 'created_at')
     search_fields = ('name', 'description')
     list_filter = ('level', 'school', 'created_at')
+
+@admin.register(Ability)
+class AbilityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'related_spell', 'created_at')
+    search_fields = ('name', 'description')
+    list_filter = ('type', 'created_at')
