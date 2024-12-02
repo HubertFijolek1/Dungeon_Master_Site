5d4a47082a9554a14e0ca4c411a5f6263cbbf9a5
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-12-02 12:41:40 +0100

    Register Spell model in admin with customized interface.

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Spell
 
-# Register your models here.
+@admin.register(Spell)
+class SpellAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'school', 'created_at')
+    search_fields = ('name', 'description')
+    list_filter = ('level', 'school', 'created_at')
